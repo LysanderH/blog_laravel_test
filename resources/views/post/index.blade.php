d9f659d72a53743963fa7886dad20fb06d592fb3
Lysander <lysander.hans@hotmail.com>
2020-09-24 11:16:59 +0200

    📦 Created main view

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,22 +1,16 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
-</head>
-<body>
-<h1>La liste des posts</h1>
-<ul>
-    @if(count($posts))
-        @foreach($posts as $post)
-            <li><a href="/posts/{{$post->slug}}">{{$post->title}} ({{$post->comments_count}})</a></li>
-        @endforeach
-    @else
-        <p>Il n'y a pas de posts sur le site</p>
-    @endif
-</ul>
-</body>
-</html>
+@extends('layouts.main')
+@section('title')
+    <title>Liste des artilces</title>
+@endsection
+@section('content')
+    <h1>La liste des posts</h1>
+    <ul>
+        @if(count($posts))
+            @foreach($posts as $post)
+                <li><a href="/posts/{{$post->slug}}">{{$post->title}} ({{$post->comments_count}})</a></li>
+            @endforeach
+        @else
+            <p>Il n'y a pas de posts sur le site</p>
+        @endif
+    </ul>
+@endsection
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,52 +1,46 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.main')
+@section('title')
     <title>{{$post->title}}</title>
-</head>
-<body>
-<h1>{{$post->title}}</h1>
-<p>{{$post->body}}</p>
+@endsection
+@section('content')
+    <h1>{{$post->title}}</h1>
+    <p>{{$post->body}}</p>
 
-@if(count($comments))
-    <ul>
-        @foreach($comments as $comment)
-            <li>
-                {{$comment->body}}
-            </li>
-        @endforeach
-    </ul>
-@else()
-    <p>Pas de commentaires, soyer le premier à commenter</p>
-@endif
+    @if(count($comments))
+        <ul>
+            @foreach($comments as $comment)
+                <li>
+                    {{$comment->body}}
+                </li>
+            @endforeach
+        </ul>
+    @else()
+        <p>Pas de commentaires, soyer le premier à commenter</p>
+    @endif
 
-<form action="/comments" method="post">
-    @csrf
-    <label for="body">Contenu de votre commentaire</label>
-    <textarea id="body" name="body"></textarea>
-    @error('body')
-    <p>{{$message}}</p>
-    @enderror
-    <input type="hidden" name="post_id" value="{{$post->id}}">
-    @error('post_id')
-    <p>Don't touch!!!</p>
-    @enderror
-    <input type="hidden" name="post_slug" value="{{$post->slug}}">
-    <input type="submit" value="Commenter ce post">
-</form>
-{{--@if ($errors->any())--}}
-{{--    <div class="alert alert-danger">--}}
-{{--        <ul>--}}
-{{--            @foreach ($errors->all() as $error)--}}
-{{--                <li>{{ $error }}</li>--}}
-{{--            @endforeach--}}
-{{--        </ul>--}}
-{{--    </div>--}}
-{{--@endif--}}
+    <form action="/comments" method="post">
+        @csrf
+        <label for="body">Contenu de votre commentaire</label>
+        <textarea id="body" name="body"></textarea>
+        @error('body')
+        <p>{{$message}}</p>
+        @enderror
+        <input type="hidden" name="post_id" value="{{$post->id}}">
+        @error('post_id')
+        <p>Don't touch!!!</p>
+        @enderror
+        <input type="hidden" name="post_slug" value="{{$post->slug}}">
+        <input type="submit" value="Commenter ce post">
+    </form>
+    {{--@if ($errors->any())--}}
+    {{--    <div class="alert alert-danger">--}}
+    {{--        <ul>--}}
+    {{--            @foreach ($errors->all() as $error)--}}
+    {{--                <li>{{ $error }}</li>--}}
+    {{--            @endforeach--}}
+    {{--        </ul>--}}
+    {{--    </div>--}}
+    {{--@endif--}}
 
-<a href="/posts">Voir la liste des articles</a>
-</body>
-</html>
+    <a href="/posts">Voir la liste des articles</a>
+@endsection
